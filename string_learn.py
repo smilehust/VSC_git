f0aac9115b59d5072e42f83806976af1175d9925
smilehust <liweiyunxi@gmail.com>
2018-09-12 16:00:50 +0800

    注释放在行位并对齐

--- string_learn.py
+++ string_learn.py
@@ -1,6 +1,5 @@
 import string
 
 s = 'The quick brown fox jumped over the lazy dog.'
-# capwords() 将字符串中所有单词的首字母大写
-print(string.capwords(s))
-print(s.split('o'))
+print(string.capwords(s))  # capwords() 将字符串中所有单词的首字母大写
+print(s.split('o'))        # 以‘o’为分隔符分割字符串，返回一个列表
