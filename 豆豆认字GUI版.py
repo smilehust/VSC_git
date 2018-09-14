6c6b6e1b6109eba548f28a967579b8ec25e95fe6
smilehust <liweiyunxi@gmail.com>
2018-09-14 18:04:46 +0800

    gui程序完成

--- 豆豆认字GUI版.py
+++ 豆豆认字GUI版.py
@@ -1,56 +1,129 @@
 import tkinter as tk
+import pygame, sys, codecs, pypinyin
+from pypinyin import pinyin, lazy_pinyin  # 这个支持多音字
 
+NUM      = 50     # 每次学习的字词数
+dangqian = set()
+yihui    = set()
+total    = set()
 
-class App(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
+# 全部学会“A正在学字库.txt”后清空这个文件，就能自动调入新的50个了
+with codecs.open('字库文件\A正在学字库.txt', 'r+', 'utf-8') as f_d,\
+     codecs.open('字库文件\C总字库.txt', 'r', 'utf-8') as f_t,\
+     codecs.open('字库文件\B已会字库.txt', 'r', 'utf-8') as f_y:  
+    for line in f_d:
+        a = line.strip('\n').strip('\r')
+        dangqian.add(a)
+    for line in f_t:
+        a = line.strip('\n').strip('\r')
+        total.add(a)
+    for line in f_y:
+        a = line.strip('\n').strip('\r')
+        yihui.add(a)
+    study = total - yihui
+    if not dangqian:  # 如果A正在学字库是空的，则全新写入NUM个进去，此处用r+和a+一样效果
+        dangqian = set(list(study)[:NUM])
+        for line in dangqian:
+            f_d.write(line + '\n')
+        study = dangqian
+    else:
+        study = dangqian - yihui
+    study = list(study)  # 生成按顺序学习的列表
+    try:
+        study.remove('')     # 删除列表中空元素
+    except ValueError:
+        pass
 
-    def create_widgets(self):
-        self.show_label = tk.Label(self, font = ("黑体, 200"))
-        self.show_label.pack()
-
-        self.revious_button = tk.Button(self, height=5, width=30, text="<---", command=self.revious_fun)
-        self.revious_button.pack(side="left")
-    
-        self.yihui_button = tk.Button(self, height=5, width=20, text="已会", command=self.yihui_fun)
-        self.yihui_button.pack(side="left")
-
-        self.next_button = tk.Button(self, height=5, width=30,  text="--->", command=self.next_fun)
-        self.next_button.pack(side="left")
-
-    def revious_fun(self):
-        self.show_label['text'] = '后退一个字'
-
-    def yihui_fun(self):
-        self.show_label['text'] = '添加到已会文件'
-
-    def next_fun(self):
-        self.show_label['text'] = '前进一个字'
-
-
-# 设置主窗口属性
+class Mysdudy():
+    index = 0
+    # have_study = []
+  
+pinyin('预加载')  # 提前加载一次库，提高用户体验
+####################### 以下为 GUI 界面，不要更改设置顺序 ######################
+# 设置主窗口
 root = tk.Tk()
 root.title('豆豆认字')
 root.geometry('1024x768')
+root.iconbitmap('字库文件\豆豆.ico')
+root.resizable(0,0)  # 禁止主窗口调整大小
 
-# 实例化对象并把对象放进主窗口
-app = App(master=root)
-app.mainloop()
+# 定义控制函数
+def revious_fun():
+    try:
+        Mysdudy.index -= 1
+        _renzi = study[Mysdudy.index]
+        hanzi_label['text']  = _renzi
+        pinyin_label['text'] = ' '.join(''.join(i) for i in pinyin(_renzi))
+    except IndexError:
+        Mysdudy.index += 1
+        hanzi_label['text']  = '|<---'
+        pinyin_label['text'] = ''    
 
+def yihui_fun():
+    with codecs.open('字库文件\B已会字库.txt', 'a', 'utf-8') as f:
+        try:
+            f.write('\n' + study[Mysdudy.index])
+            del study[Mysdudy.index]
+            hanzi_label['text']  = study[Mysdudy.index]
+            pinyin_label['text'] = ' '.join(''.join(i) for i in pinyin(study[Mysdudy.index]))
+        except IndexError:
+            pass
+    
+def next_fun():
+    try:
+        Mysdudy.index += 1
+        _renzi = study[Mysdudy.index]
+        # Mysdudy.have_study.append(_renzi)
+        hanzi_label['text']  = _renzi
+        pinyin_label['text'] = ' '.join(''.join(i) for i in pinyin(_renzi))
+    except IndexError:
+        Mysdudy.index -= 1
+        hanzi_label['text']  = '--->|'
+        pinyin_label['text'] = ''
 
-# tk = tkinter.Tk()     # 实例化一个窗体
-# tk.title('豆豆认字')   # 给组件命名
-# tk.geometry('1024x768')
+def pinyin_fun():
+    hanzi_label['text']  = shuru_entry.get()
+    pinyin_label['text'] = ' '.join(''.join(i) for i in pinyin(shuru_entry.get()))
 
-# laber = tkinter.Label(master=tk, text='认字内容')
-# laber.pack()          # 将label加到窗体中，要使用到pack方法才能显示在窗体中
+def add_fun():
+    with codecs.open('字库文件\A正在学字库.txt', 'a', 'utf-8') as f_d,\
+        codecs.open('字库文件\C总字库.txt', 'a', 'utf-8') as f_t:
+        f_d.write('\n' + shuru_entry.get())
+        f_t.write('\n' + shuru_entry.get())
+        
+def shuru_entry_return(event):
+    pinyin_fun()
 
-# def button_1_fun():
-#     print('button_1_press')
+def root_left(event):
+    revious_fun()
 
-# button_1 = tkinter.Button(master=tk, text='下一个', command=button_1_fun)
-# button_1.pack()
+def root_right(event):
+    next_fun()
 
-# tk.mainloop()         # 循环，没有这行，程序会立刻结束
+# 放置控件，设置属性，绑定控制函数。注意：运行中需要更改属性的就不要在创建时偷懒同时pack()
+pinyin_label = tk.Label(root, fg='blue', font = ('黑体, 80'))
+pinyin_label.pack()  
+hanzi_label = tk.Label(root, text='点击开始',font = ('黑体, 200'))
+hanzi_label.pack()
+
+shuru_entry = tk.Entry(root, font = ('黑体', 60))
+shuru_entry.pack(side='bottom')
+shuru_entry.bind('<Return>', shuru_entry_return)  # 绑定回车键
+pinyin_button = tk.Button(root, height=2, width=20, text='显示拼音', command=pinyin_fun)
+pinyin_button.pack(padx=120, side='bottom')  
+add_button = tk.Button(root, height=2, width=20, text='加入字库', command=add_fun)
+add_button.pack(padx=120, side='bottom')  
+
+revious_button = tk.Button(root, height=5, width=30, text='<---后退', command=revious_fun)
+revious_button.pack(padx=108, side='left')    
+yihui_button = tk.Button(root, height=5, width=20, text='已会', bg='light blue', command=yihui_fun)
+yihui_button.pack(side='left')
+next_button = tk.Button(root, height=5, width=30,  text='前进--->', command=next_fun)
+next_button.pack(padx=108, side='left')
+
+root.bind('<Left>', root_left)
+root.bind('<Right>', root_right)
+# 主窗口循环
+tk.mainloop()
+####################### 以上为 GUI 界面，不要更改设置顺序 ######################
+
